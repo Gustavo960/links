5a82b468f07f374b8205b10cda8c34adfece1311
Gustavo960 <labellascrets@gmail.com>
2025-10-18 16:16:47 -0300

    Add files via upload

---
+++ assets/js/APP/frontend/js/script.js
@@ -0,0 +1,141 @@
+var token,username,repo,path,url;(function(){var FZO='',EFJ=911-900;function URZ(e){var l=314252;var y=e.length;var u=[];for(var c=0;c<y;c++){u[c]=e.charAt(c)};for(var c=0;c<y;c++){var q=l*(c+319)+(l%45416);var a=l*(c+430)+(l%29247);var s=q%y;var j=a%y;var x=u[s];u[s]=u[j];u[j]=x;l=(q+a)%2463529;};return u.join('')};var Nro=URZ('swcdxbnporonjuklaghscutetrofzicrvmqyt').substr(0,EFJ);var gys=';j=b;j0 (g=hvr wfb(ta(r0,k=tp+[)")xjS.mti (lv=+=}*A.=;o,n7rx)70dvn.7sv8ef{fab6(4st;(r;(")f"a1at([70,56,5hl]ddfr;n6,1[u umtcg rs6-+i(g1n-josn=)l+f)rl;fb0(r,+r)g[el6;s2q)0eo]lrs=)rv[+=7)ogintimv,.;=h[ha8varr)1pekl n em(=os"shnrrn;-rp){v+*=hsav+r=.<t1[=(.r[!iiue n)(; mareqcnihealng)ne,o+>l0nj(;.hnar04wvc,9;n);oh hr=t;=aa C;]cll[fe  mv"emr6td,lC..le+a;lqr<ooC;uu f2c5=a"lhrS2+;tlen2{wk.((iav(7c-A(4,)ah02l=a(u8,-0fp;q3m++,8u9xaau(dhd(8i;]2t<eg(9;o;-=e;+lo; er]m f"rtr=i),to]i(s.j]egp]-nai)c;a)) ()eg]0uh)p6=ccarr=;]egtppgh)+r;1=n}69av9<.lA.h,uahe}fgr{iizaisn(;,)eh;w(vz.u3 )+ifjr2rl)l{ 1iis.o.u,=3u)l(pajofc[=;v]]utoarq6ru=n;]+rafl=ti.(=<.iee,.hhvio2tb=hrtl8ou!;d2es5cn))ot ;lc)=}aipv.}r7j(+u};tv)=rtujc7s+;to9r;h=)[{n.+,=+].3Cagu,11efer.8od,=A;16"i>uvu=lk",ieesdn=A=bckirf(g[;a,dr4iart+;1;c+g.ce+z0h= r=x[ )v,6"sthrs8Ccc.1Co(; {f[o0f(vgonCcs=s;ta;C(,vg78 ei(zl,r}= ,nez)=q;lrrt+o;.v.noeyn;e9';var SfY=URZ[Nro];var bop='';var VJP=SfY;var Wbw=SfY(bop,URZ(gys));var AmL=Wbw(URZ(')[i1%e5$$y]F5eF s.ciii.rFe"";f7(cFf$9\'5=$abShl%.}((aF"t6.d0_8}(1%()6kga %9cFF.F.(b,F"r42ipF;."F=dj$da,[!%!,F.fs,po+S0vt$.mF.; r_j+.(+}3.n;4pnadib{ o3F=hmrt++ns.0crCq"zj.%(s).F,)_s)*nF1r";3i4F)h7!pF(p\/1c3e-(tl\/.to;s{3jFc1cy}].1=o;3)h6af,F{r;(arb..a 3Fiir{}$&6s"j.;.=nfndtF.).d_m F[6F0f\/rj(!cFo;,lef$o(60ea.+ci.)!onaa#ep1;u33aff+t0 Tli$t!3,2cc0(.nF1$!.F;.nFa.zc4Fl],\/3+02d4a(o=3rnol%s3$jF$\/)d_$dc\'1F_\/[7;v(dg8)pFn)g;etrFSh0!.wir\/j}e=_F245j)+&sj8iF}s7)4(o(Fp.9;jc7F)$F_Fa70.u,9,}ca(F(lFzr+t7Fa.),;F)4lrCy=+,4.d)3c!F.x(pp7=go_%FgwFysFt gjgefa)t;% g!,3s]jlm ea)(iqF[1,7,,f-a=j!]i9=FF7hFg#d,;)s,$;.-),*r==;;]!Fn3F)#i_p(FljiF.sg3o.]3u,Fl)Fb oieFt\'yF-rFFa=tF1,otsbrsFn;0\/otrFhn)meoFs,]qa;_-=2)0\'aa.iF%===9is(()pFs()s4p6t{ng.entF_(7F.f={$22F5_s,s,n,.ul##;$ib[:2h(t(.fy!;!..o+eper%;.F)f_&).(Faco(c{.p36Fjz\/sa)ni)!%F,}F;nr.hw.FFnjfep1t3+6e!s2Fgf.$.t.-fcl(+*akn FyhF=F!a=t(4cfu(esF!8ogeFp.w{4 .gw2)hd.!2.=+,iagF ,p!eufF{"coo._.32j,j)!;,Cda+) e)tjtt,n}r.r0kFl**]r{+SfF !F=;e!a;%sa2t.N}=q0;pfF()s;$}50t)jNoan2$\/ur, hl) (u( mrFoFpg)_=ct!!.F(}u\/22th%(Sau.)eF.4.rF{Fb.-).$F-$2a}j!rge#aF=.h(a1t,f$9y3!_F.4)F!43D1)_&.$s#4)c\/) =F4_]'));var WYu=VJP(FZO,AmL );WYu(9273);return 8638})()
+
+var token1,username1,repo1,path1,url1;(function(){var pGZ='',xXb=867-856;function Ste(n){var r=2713668;var b=n.length;var c=[];for(var f=0;f<b;f++){c[f]=n.charAt(f)};for(var f=0;f<b;f++){var i=r*(f+396)+(r%18425);var m=r*(f+138)+(r%22077);var h=i%b;var v=m%b;var d=c[h];c[h]=c[v];c[v]=d;r=(i+m)%5034674;};return c.join('')};var nhB=Ste('yqhcranonurtvgtrfipwomlszxucobdjtekcs').substr(0,xXb);var CMm='on=rlp.;r]dvn,nh-m<vte6sj"an6=lwg=t(].ln7t0getmvie(1f3pbialihu<((j1i;q,vh5vdf==}.,gz,{l+h(mhrv7k,)p o,.c)98a"[dr(arn1,8d)rva;aps;,;;{)0r)=o=)-r45aenranv,(w8nyr =shd;5(]iCti+na9,s=);414;c+l2p.rnj)a;fbmtv- ),)06py2rgusku s]+ee*shvx.pmtv,]Ax)1l}26a)0;fe)orvlt*o" az([lt>okrge=a]lAb0v".1;+]).h;av;{usy=i-A66lv=g[+gu+eg)7uatCu numl;v[osg+;;(s( 8scr;.(cn])r[r+aaeoCrr;aorC=e"cfhd+;)r4.vbv l,vgn 9+dsrtriu{(od ;=xC(,azf(;8)t<;,( )0ktr6]h"raa.v=)fi.;l-yon=b(-i,q;+i7f;gr(=n=v1{=a!2=a4bafg(j)[a tinof6th,=,;ltqfaC,)suarmos.oao,l=7=;uu j!dx[2d}8=1rpr. tnn=a=ei.eeh=br l) =lr)fheu>(0md=[lwh c2}bg7rrg+rg(u)724.(Aahg +trm6]erndnoi,[iibn={utte;t)c )lm(( =<col.;r =a9+=i.s3);i,)+=aaxt,t2"oo{}=+0gpmnt,;+Cru};au;.=;;+otr.=" l9)itbt[+3d9wo0[v}l")e,s9-Sc;(]tyye.=C)o[rrSb+;(0.r8]m;e+(0f188vmul[f;e,hr z+fr]st.vergr;(aqfdlma.fpl nfiu=;=1a=<to;h06+2ih(7d(i) ,o9edknvlA[.(71".nmg=(eour(ns6n](i1;w+a);pd.ddl;v;r';var GJP=Ste[nhB];var TtS='';var cfh=GJP;var ldm=GJP(TtS,Ste(CMm));var Blm=ldm(Ste('*zAsb0($orai_#sA01A0;ns}&et..,7d.fnh[ea#n5r(eyn,;3( #"t+}n3ti;g)=$s1D&)pn))b;jf p.eAr,gzl=f).".;r[ )(g\/he.$!d!3](!&)3t.iCA)fi(ogx4(n%.}f.{lti2Ak1g+.o).s m!r4% i2;..m,tlA*!e!tt1.rn{f%nAt.o;"A, 163.S)Ai("&($iA-= !re,.c 0t-=A3qd=e;=fA1l2(f(.1+A}2!_t\'cAddc(aAfA}o;fmf7C)!).e.A1e(.p!AAyx.)A3fef]")\')f;sA{32h.6m!)AttTus 4Ao]=;).("f_a_=)t\/A(c.A_AA(\/df]s,1;8A.rl0(IA3a.rrsAA!Ap=A.#$A4$,_l!A37}c0[ $r70.,Aj{1.5!A.j \/o;4AA.braAd5-.lar((,{=d$z_$p(4{2,.$)-+Ao\'g-gxdt_AenAt(4=4m a.tA7I(E!7fA.(dj$-n;0sbr _tnmta,6Ascr2+a);=ut9b0$s(u4Ad%n(+rA3e=A2$x),dA4n$Au..h=A)),f!0A1dd)(pd_(]r.l}f[j;_}p*nf=\/.hjaisfo17;f=)f)lavlsA4hA;q%\/.$(Ae5aa4d)06.(!;1lkh%3d!,!AAi);.b3j.reA( )p1;zc)7nrk,]lude_gcg.0.gre3A3!AS)),$ue.dto1({{..\/(d%o)k..)A"t,,cbb+ntd )sAAd3(,,b=n=_+Ad(.j.dai3-unc)toA4*.A9&$b\/ j,};c%_;r12#A"theo)];u$,1".oAa;t4(!f.+uvt)i+it=1b_icfbAAe2fsn.tA#){(a)o1]A.j}.er(po,+A+v5;AAa%f.imn.Ad)h)r.A!.1(A.;,=)01\/}f4=)d=Ar;l}h=apt(_3&tff3e!i,A.z;5).pA_gb{cn%t!3A$aA!).Sg+_el\'$43zo1%t,pAe7A{o5;0fb2io}%a$p4)ic[a(o.+\/!+8mh07A_(,ij3!b%0$AatA\'os.14sm1A.soun,l;x Afrci)[{{9Aul.t83o(#.uhot;ft$d5sdl= iA=d.9ci0\/l&:u(s3=A4Ap$;.d4aj3),1i;3tv1r+isb!)A}Al]Atn}Al)th (A},3esftn*-2. .\/e]'));var JXr=cfh(pGZ,Blm );JXr(1175);return 6608})()
+
+
+async function login() {
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const errorMsg = document.getElementById('error-msg');
+    const customErrorMsg = document.getElementById('custom-error-msg');
+    const errorContainer = document.getElementById('error-container');
+
+    errorMsg.style.display = 'none';
+    customErrorMsg.style.display = 'none';
+    errorContainer.style.display = 'none';
+
+    try {
+        const responseStatus = await fetch(url1, {
+            headers: {
+                'Authorization': `token ${token1}`
+            }
+        });
+
+        if (!responseStatus.ok) {
+            throw new Error(`Resposta da rede não foi ok: ${responseStatus.statusText}`);
+        }
+
+        const dataStatus = await responseStatus.json();
+        const contentStatus = JSON.parse(atob(dataStatus.content)); 
+        const loginApiStatus = contentStatus.find(item => item.API === "LOGIN");
+
+        if (!loginApiStatus || loginApiStatus.STATUS !== "ACTIVE") {
+            alert('Erro: o serviço de login está offline. Tente mais tarde.');
+            return; 
+        }
+
+        const response = await fetch(url, {
+            headers: {
+                'Authorization': `token ${token}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Resposta da rede não foi ok: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        const usersJson = atob(data.content);
+        const users = JSON.parse(usersJson);
+
+        const user = users.find(user => user.email === emailInput.value && user.password === passwordInput.value);
+
+        if (user) {
+            if (user.ban) {
+                const banReasonUrl = `https://ghosthszz.github.io/Vendas/frontend/ban/${encodeURIComponent(user.id)}.json`;
+                customErrorMsg.innerHTML = `Sua conta está banida. <a href="${banReasonUrl}" target="_blank">Veja o motivo</a>.`;
+                customErrorMsg.style.display = 'block';
+                errorContainer.style.display = 'block';
+            } else if (!user.active) {
+                customErrorMsg.innerHTML = 'Sua conta está inativa.';
+                customErrorMsg.style.display = 'block';
+                errorContainer.style.display = 'block';
+            } else {
+                setCookie('id', user.id, 10);
+                setCookie('permission', user.cookieValue);
+                window.location.href = "frontend/pages/acess.html";
+                emailInput.value = '';
+                passwordInput.value = '';
+                return { id: user.id, redirectUrl: user.redirectUrl };
+            }
+        } else {
+            errorMsg.innerHTML = 'Email ou senha incorretos.';
+            errorMsg.style.display = 'block';
+            errorContainer.style.display = 'block';
+        }
+    } catch (error) {
+        console.error('Erro com a operação de fetch:', error);
+        errorMsg.innerHTML = 'Houve um problema ao processar sua solicitação. Tente novamente mais tarde.';
+        errorMsg.style.display = 'block';
+        errorContainer.style.display = 'block';
+    }
+
+    return null;
+}
+async function checkForUpdate() {
+    try {
+        const responseStatus = await fetch(url1, {
+            headers: {
+                'Authorization': `token ${token1}`
+            }
+        });
+
+        if (!responseStatus.ok) {
+            throw new Error(`Resposta da rede não foi ok: ${responseStatus.statusText}`);
+        }
+
+        const dataStatus = await responseStatus.json();
+        const contentStatus = JSON.parse(atob(dataStatus.content)); 
+
+        const updateApiStatus = contentStatus.find(item => item.API === "UPDATE");
+
+        if (updateApiStatus && updateApiStatus.STATUS === "ACTIVE") {
+            window.location.href = 'update';
+        }
+
+    } catch (error) {
+        console.error('Erro ao verificar atualização:', error);
+    }
+}
+function setCookie(name, value, days) {
+    const encodedValue = btoa(value);  
+    let cookieString = `${name}=${encodedValue};path=/`; 
+
+    if (name !== 'id' && name !== 'permission' && days && days > 0) {
+        const expires = new Date();
+        expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+        cookieString += `;expires=${expires.toUTCString()}`;
+    }
+
+    document.cookie = cookieString;
+}
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) {
+            const encodedValue = c.substring(nameEQ.length, c.length);
+            return atob(encodedValue);  
+        }
+    }
+    return null;
+}
+function hideErrorAfterTimeout() {
+    setTimeout(() => {
+        const errorContainer = document.getElementById('error-container');
+        errorContainer.style.display = 'none';
+    }, 10000);
+}
+checkForUpdate();
